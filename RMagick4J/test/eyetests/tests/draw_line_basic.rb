d556eab795ab38111ae84fff6ed620e8db7e4722
serabe <serabe@2e09a4fa-c62d-0410-8269-838b47078f26>
2008-05-30 21:04:19 +0000

    Added two more test. There is a little problem with colors and shapes.
    git-svn-id: https://rmagick4j.googlecode.com/svn/trunk@98 2e09a4fa-c62d-0410-8269-838b47078f26

---
+++ RMagick4J/test/eyetests/tests/draw_line_basic.rb
@@ -0,0 +1,20 @@
+require File.join(File.dirname(__FILE__), 'new_image.rb')
+
+include Magick
+
+draw = Draw.new
+
+draw.stroke = 'red'
+draw.stroke_width 10.0
+
+draw.line(50, 50, 50, 200)
+draw.stroke('blue')
+draw.line(50, 200, 200, 200)
+draw.stroke('green')
+draw.line(200, 200, 50, 50)
+
+b = Image.new(300, 300, HatchFill.new('white', 'black'))
+
+draw.draw b
+
+b.write('draw_line_basic.jpg')
---
+++ RMagick4J/test/eyetests/tests/draw_polygon_basic.rb
@@ -0,0 +1,19 @@
+require File.join(File.dirname(__FILE__), 'new_image.rb')
+
+include Magick
+
+draw = Draw.new
+
+draw.stroke = 'blue'
+draw.stroke_width 5.0
+draw.fill = 'red'
+
+points = [350,75,379,161,469,161,397,215,423,301,350,250,277,301,303,215,231,161,321,161].collect { |x| x/2.round }
+
+draw.polygon(*points)
+
+b = Image.new(300, 300, HatchFill.new('white', 'black'))
+
+draw.draw b
+
+b.write('draw_polygon_basic.jpg')
