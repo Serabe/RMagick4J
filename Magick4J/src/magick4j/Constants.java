10dba44a129f1664d5b5fa4d1d3a3fce8a49b078
Sergio Arbeo <serabe@gmail.com>
2010-02-06 22:46:58 +0100

    I forgot to save the constants file.

--- Magick4J/src/magick4j/Constants.java
+++ Magick4J/src/magick4j/Constants.java
@@ -13,4 +13,5 @@ public class Constants {
 	public static final double QuantumRange = 255.0;
 	public static final double MagickEpsilon = 1.0e-10;
 	public static final double MagickHuge = 1.0e12;
+	public static final double MagickSQ2PI = 2.50662827463100024161235523934010416269302368164062;
 }
