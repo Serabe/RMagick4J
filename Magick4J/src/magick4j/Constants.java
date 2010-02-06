1f02a6660cc7d691c533010e4b00e1b50ad82795
Sergio Arbeo <serabe@gmail.com>
2010-02-06 22:44:56 +0100

    Added method to find optimal kernel for convolution (1D).

---
+++ Magick4J/src/magick4j/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package magick4j;
+
+/**
+ *
+ * @author serabe
+ */
+public class Constants {
+	public static final double QuantumRange = 255.0;
+	public static final double MagickEpsilon = 1.0e-10;
+	public static final double MagickHuge = 1.0e12;
+}
---
+++ Magick4J/src/magick4j/Convolution.java
@@ -0,0 +1,35 @@
+package magick4j;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.ceil;
+import static java.lang.Math.exp;
+/**
+ *
+ * @author serabe
+ */
+public class Convolution {
+	/*
+	 * Ported from ImageMagick
+	 * 
+	 */
+	public static long getOptimalKernelWidth1D(double radius, double sigma){
+		if(radius > 0.0)
+			return (long) (2.0*ceil(radius)+1.0);
+		if(abs(sigma) <= Constants.MagickEpsilon)
+			return 1;
+		long width, u;
+		double normalize, value;
+
+		for(width=5;;){
+			normalize = 0.0;
+			for (u=(-width/2); u <= (width/2); u++)
+				normalize+=exp(-((double) u*u)/(2.0*sigma*sigma))/(Constants.MagickSQ2PI*sigma);
+			u=width/2;
+			value=exp(-((double) u*u)/(2.0*sigma*sigma))/(Constants.MagickSQ2PI*sigma)/normalize;
+			if ((long) (Constants.QuantumRange*value) <= 0L)
+				break;
+			width+=2;
+		}
+		return width-2;
+	}
+}
