55b56286b47f43afe8641df49d9f89c7fb6e6e95
Karl Palmskog <palmskog@kth.se>
2010-09-13 17:11:15 +0200

    added support for CMYK JPEG images in MagickImage, using code for conversion to RGB from http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4799903

--- Magick4J/src/magick4j/MagickImage.java
+++ Magick4J/src/magick4j/MagickImage.java
@@ -3,7 +3,6 @@ package magick4j;
 import static java.lang.Math.min;
 import static java.lang.Math.max;
 
-
 import java.awt.AlphaComposite;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -12,27 +11,39 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.Transparency;
+import java.awt.color.ColorSpace;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BandCombineOp;
 import java.awt.image.BufferedImage;
+import java.awt.image.ComponentColorModel;
+import java.awt.image.ColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.util.Iterator;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
+import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.metadata.IIOMetadataNode;
 import javax.imageio.stream.ImageInputStream;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
+import org.w3c.dom.NodeList;
 
 public class MagickImage implements Cloneable {
 
@@ -721,36 +732,144 @@ public class MagickImage implements Cloneable {
     }
 
     private void readImage(Object input) {
-        try {
-            ImageInputStream stream = ImageIO.createImageInputStream(input);
-            if (stream == null) {
-                throw new RuntimeException("failed to open " + input);
-            }
-            try {
-                for (Iterator<ImageReader> readers = ImageIO.getImageReaders(stream); readers.hasNext();) {
-                    ImageReader reader = readers.next();
-                    try {
-                        reader.setInput(stream);
-                        format = reader.getFormatName().toUpperCase();
-                        //image = reader.read(0);
-			BufferedImage pre = reader.read(0);
-			image = new BufferedImage(pre.getWidth(), pre.getHeight(), BufferedImage.TYPE_INT_ARGB);
-			image.createGraphics().drawImage(pre, 0, 0, null);
-			image.createGraphics().dispose();
-			pre = null;
-                        backgroundColor = ColorDatabase.lookUp("white");
-                        // TODO Read multiple images if present? How to coordinate this and ImageList?
-                        break;
-                    } finally {
-                        reader.dispose();
-                    }
-                }
-            } finally {
-                stream.close();
-            }
-        } catch (Exception e) {
-            Thrower.throwAny(e);
-        }
+	try {
+	    ImageInputStream stream = ImageIO.createImageInputStream(input);
+	    if (stream == null) {
+		throw new RuntimeException("failed to open " + input);
+	    }
+	    try {
+		for (Iterator<ImageReader> readers = ImageIO.getImageReaders(stream); readers.hasNext();) {
+		    ImageReader reader = readers.next();
+		    try {
+			reader.setInput(stream);
+			format = reader.getFormatName().toUpperCase();			
+			if ("JPEG".equalsIgnoreCase(format) || "JPG".equalsIgnoreCase(format)) {
+			    readJPEG(reader);
+			} else { // not a JPEG, read normally
+			    drawImageFromReader(reader);
+			}
+			backgroundColor = ColorDatabase.lookUp("white");
+			// TODO Read multiple images if present?
+			// How to coordinate this and ImageList?
+			break;
+		    } finally {
+			reader.dispose();
+		    }
+		}
+	    } finally {
+		stream.close();
+	    }
+	} catch (Exception e) {
+	    Thrower.throwAny(e);
+	}
+    }
+
+    private void drawImageFromReader(ImageReader reader) throws IOException {
+	BufferedImage pre = reader.read(0);
+	image = new BufferedImage(pre.getWidth(), pre.getHeight(), BufferedImage.TYPE_INT_ARGB);
+	image.createGraphics().drawImage(pre, 0, 0, null);
+	image.createGraphics().dispose();
+	pre = null;
+    }
+
+    private void readJPEG(ImageReader reader) throws IOException {
+	Iterator<ImageTypeSpecifier> imageTypes = reader.getImageTypes(0);
+	if (imageTypes.hasNext() && imageTypes.next().getNumBands() == 3) { 
+	    // RGB JPEG, read normally
+	    drawImageFromReader(reader);
+	} else { // might be CMYK or YCCK JPEG
+	    IIOMetadata metadata = reader.getImageMetadata(0);
+	    String metadataFormat =
+		metadata.getNativeMetadataFormatName();
+	    IIOMetadataNode iioNode = (IIOMetadataNode)
+		metadata.getAsTree(metadataFormat);
+	    NodeList children =
+		iioNode.getElementsByTagName("app14Adobe");
+
+	    if (children.getLength() > 0) { // has colorspace field		
+		iioNode = (IIOMetadataNode) children.item(0);
+		int transform = Integer.
+		    parseInt(iioNode.getAttribute("transform"));
+		Raster raster = reader.readRaster(0, reader.getDefaultReadParam());
+		
+		if (raster.getNumBands() == 4) { // CMYK or YCCK
+		    image = createJPEG4(raster, transform);
+		} else { // wrong number of channels
+		    throw new RuntimeException("failed to process jpeg");
+		}
+	    } else { // no app14Adobe field, read normally
+		drawImageFromReader(reader);
+	    }	    
+	}
+    }
+
+    /**
+     * Java's ImageIO can't process 4-component images
+     * and Java2D can't apply AffineTransformOp either,
+     * so convert raster data to RGB.
+     *
+     * Technique due to Mark Stephens.
+     *
+     * Free for any use.
+    */
+    private static BufferedImage createJPEG4(Raster raster, int xform) {
+	int w = raster.getWidth();
+	int h = raster.getHeight();
+	byte[] rgb = new byte[w*h*3];
+
+	// if Adobe_APP14 and transform == 2 then YCCK else CMYK
+	if (xform == 2) { // YCCK
+
+	    float[] Y = raster.getSamples(0, 0, w, h, 0, (float[]) null);
+	    float[] Cb = raster.getSamples(0, 0, w, h, 1, (float[]) null);
+	    float[] Cr = raster.getSamples(0, 0, w, h, 2, (float[]) null);
+	    float[] K = raster.getSamples(0, 0, w, h, 3, (float[]) null);
+
+	    for (int i = 0, imax = Y.length, base = 0; i < imax; i++, base += 3) {
+		float k = 220 - K[i], y = 255 - Y[i], cb = 255 - Cb[i], cr = 255 - Cr[i];
+
+		double val = y + 1.402*(cr - 128) - k;
+		val = (val - 128) * .65f + 128;
+		rgb[base] = val < 0.0 ? (byte) 0 : val > 255.0 ? (byte) 0xff : (byte) (val + 0.5);
+
+		val = y - 0.34414*(cb - 128) - 0.71414*(cr - 128) - k;
+		val = (val - 128) * .65f + 128;
+		rgb[base + 1] = val < 0.0 ? (byte) 0 : val > 255.0 ? (byte) 0xff : (byte) (val + 0.5);
+
+		val = y + 1.772*(cb - 128) - k;
+		val = (val - 128) * .65f + 128;
+		rgb[base + 2] = val < 0.0 ? (byte) 0 : val > 255.0 ? (byte) 0xff : (byte) (val + 0.5);
+	    }
+
+	} else { // CMYK
+
+	    int[] C = raster.getSamples(0, 0, w, h, 0, (int[]) null);
+	    int[] M = raster.getSamples(0, 0, w, h, 1, (int[]) null);
+	    int[] Y = raster.getSamples(0, 0, w, h, 2, (int[]) null);
+	    int[] K = raster.getSamples(0, 0, w, h, 3, (int[]) null);
+
+	    for (int i = 0, imax = C.length, base = 0; i < imax; i++, base += 3) {
+		int c = 255 - C[i];
+		int m = 255 - M[i];
+		int y = 255 - Y[i];
+		int k = 255 - K[i];
+		float kk = k/255f;
+
+		rgb[base] = (byte) (255 - Math.min(255f, c * kk + k));
+		rgb[base + 1] = (byte) (255 - Math.min(255f, m * kk + k));
+		rgb[base + 2] = (byte) (255 - Math.min(255f, y * kk + k));
+	    }
+	}
+
+	// from other image types we know InterleavedRasters can be
+	// manipulated by AffineTransformOp, so create one of those.
+	raster = Raster.createInterleavedRaster(new DataBufferByte(rgb, rgb.length),
+						w, h, w*3, 3, new int[] {0, 1, 2}, null);
+
+	ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_sRGB);
+	ColorModel cm = new ComponentColorModel(cs, false, true,
+	  Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
+	return new BufferedImage(cm, (WritableRaster) raster, true, null);
     }
 
     public MagickImage resized(int newWidth, int newHeight){
@@ -867,7 +986,7 @@ public class MagickImage implements Cloneable {
         } catch (Exception e) {
             Thrower.throwAny(e);
         }
-    }
+    }    
 
     private void writeImage(String type, OutputStream stream) {
         try {
